b0584e8af042ada12396c1ec346d87a725fae249
ismay <ismay@ismaywolff.nl>
2020-09-03 08:04:59 +0000

    410dc7b fix: fix translation

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.8c501c6d3b386b50e7320c79cc325245.js"
+  "./precache-manifest.940e0b22fe82366fb624c6b42e9d583c.js"
 );
 
 self.addEventListener('message', (event) => {
