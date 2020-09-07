b84ce8e3aaa4e2cc7d45156e8c34b1e1e03e0c1c
ismay <ismay@ismaywolff.nl>
2020-09-07 12:47:46 +0000

    9f25dbf fix: use specified button order

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.f6bc3d5b4157eed740973a97341c4d51.js"
+  "./precache-manifest.e33416838fc4f0e20bf3b43d9e8030b2.js"
 );
 
 self.addEventListener('message', (event) => {
