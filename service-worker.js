3e226dbbb9c29b0ec6fe41c52ff1a6b0823a307f
Jan-Gerke Salomon <Mohammer5@users.noreply.github.com>
2020-09-17 12:35:28 +0000

    a7049ac fix(new sms cmd form): use correct value for program registration

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.72959dcdd36d79bce091ec6e8979992f.js"
+  "./precache-manifest.5ab4ebd756d5c42939b36248d6bd1057.js"
 );
 
 self.addEventListener('message', (event) => {
