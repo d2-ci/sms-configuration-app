4a309822283f2be8fd15694b5e305087069b7f3b
ismay <ismay@ismaywolff.nl>
2020-09-22 12:54:32 +0000

    98f9808 chore: update translations

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.06712a3bbeb907d5487d7b45a1da66f6.js"
+  "./precache-manifest.69beac9c7c010c055ab2b191ad6aa499.js"
 );
 
 self.addEventListener('message', (event) => {
