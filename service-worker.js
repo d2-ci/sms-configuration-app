68b4c6b661216b8da4fad8afaf6ca34d7e0f047d
Hendrik de Graaf <hendrik@dhis2.org>
2020-09-22 14:23:54 +0000

    7888f1e chore: update translations

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.69beac9c7c010c055ab2b191ad6aa499.js"
+  "./precache-manifest.df5bc924038347bf2864c8e5606fd1a9.js"
 );
 
 self.addEventListener('message', (event) => {
